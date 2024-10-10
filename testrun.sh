f7ceac03ae3f4030ee20ac5b45c49c16780ff452
jesajx <johannes.olegard@olissoft.com>
2024-10-10 22:58:08 +0200

    Minor.

--- testrun.sh
+++ testrun.sh
@@ -8,8 +8,8 @@ user_typ="5"
 placeholder="0x1234" # TODO timestamp?
 prefix="$(hexpad16 $user_typ)$(hexpad16 $placeholder)$(hexpad16 $placeholder)"
 
-#curl -H "gretel: ${prefix}$(hexpad16 0x11111111)" localhost
-curl -H "gretel: ${prefix}$(hexpad16 0x22222222)" localhost/api
+curl -vi -H "gretel: ${prefix}$(hexpad16 0x11111111)" localhost
+curl -vi -H "gretel: ${prefix}$(hexpad16 0x22222222)" localhost/api
 #curl -H "gretel: ${prefix}$(hexpad16 0x33333333)" localhost/api
 #curl -H "gretel: ${prefix}$(hexpad16 0x44444444)" localhost
 #curl -H "gretel: ${prefix}$(hexpad16 0x55555555)" localhost/api
